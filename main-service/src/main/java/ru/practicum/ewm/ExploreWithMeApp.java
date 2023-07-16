d375aa36392380c39e3f8daec69fd8d7c7afef77
Stanislav <stanislavshchekochikhin@yandex.ru>
2023-07-16 08:01:20 +0300

    add: main-service

--- main-service/src/main/java/ru/practicum/ewm/ExploreWithMeApp.java
+++ main-service/src/main/java/ru/practicum/ewm/ExploreWithMeApp.java
@@ -3,7 +3,7 @@ package ru.practicum.ewm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"ru.practicum.ewm", "ru.practicum.stats_client"})
+@SpringBootApplication(scanBasePackages = {"ru.practicum.stats_client", "ru.practicum.ewm"})
 public class ExploreWithMeApp {
     public static void main(String[] args) {
         SpringApplication.run(ExploreWithMeApp.class, args);
